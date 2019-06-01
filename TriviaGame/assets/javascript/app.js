fc48707425cd7bea56affe3f8a27f156c37e841d
Robin DuBois <46360947+Robin-DuBois@users.noreply.github.com>
2019-06-01 10:49:21 -0600

    Add files via upload
    Initial upload

---
+++ TriviaGame/assets/javascript/app.js
@@ -0,0 +1,91 @@
+let game = {
+    counter: 30,
+    correct: 0,
+    incorrect: 0,
+    questionIndex: 0,
+    source: $('#questionTemplate').html(),
+    timer: undefined,
+    start: function() {
+        this.counter = 30;
+        this.correct = 0;
+        this.incorrect = 0;
+        this.questionIndex = 0;
+        this.printQuestion(this.questionIndex);
+        this.startTimer();
+        this.gameplay();
+    },
+    startTimer: function() {
+        $('h3').text(`Time Left: 30 seconds`);
+        this.timer = setInterval(function() {
+            if (game.counter > 0) {
+                $('h3').text(`Time Left: ${game.countDown()} seconds`);
+            } else {
+                clearInterval(game.timer);
+                game.incorrectAnswer();
+            }
+        }, 1000);
+    },
+    countDown: function() {
+        this.counter--;
+        return this.counter;
+    },
+    printQuestion: function(i) {
+        let template = Handlebars.compile(game.source);
+        let context = trivia[i];
+        let html = template(context);
+        $('main').html(html);
+    },
+    gameplay: function() {
+        $('.answer').on('click', function() {
+            game.counter = 30;
+            game.compareAnswer($(this)[0].innerText[0].toLowerCase());
+        });
+    },
+    compareAnswer: function(x) {
+        if (x !== trivia[this.questionIndex].answer) {
+            this.incorrectAnswer();
+        } else {
+            this.correctAnswer();
+        }
+    },
+    correctAnswer: function() {
+        this.correct++;
+        this.questionIndex++;
+        $('#question').addClass('d-none');
+        $('#correct').removeClass('d-none');
+        clearInterval(game.timer);
+        this.timer = setTimeout(this.nextQuestion, 4000);
+    },
+    incorrectAnswer: function() {
+        this.incorrect++;
+        this.questionIndex++;
+        $('#question').addClass('d-none');
+        $('#incorrect').removeClass('d-none');
+        clearInterval(game.timer);
+        setTimeout(this.nextQuestion, 4000);
+    },
+    nextQuestion: function() {
+        if (game.questionIndex < 10) {
+            game.counter = 30;
+            game.printQuestion(game.questionIndex);
+            game.startTimer();
+            game.gameplay();
+        } else {
+            $('h3').text(`Thanks For Playing!`);
+            $('main').html(
+                `<div style="background: #00688c;"><h3>That's the end of the game. You got ${
+                    game.correct
+                } questions correct and ${
+                    game.incorrect
+                } incorrect.</h3><button class="btn btn-lg btn-block bg-warning" id="playAgain">Play Again</button></div>`
+            );
+            $('#playAgain').on('click', function() {
+                game.start();
+            });
+        }
+    }
+};
+
+$(function() {
+    game.start();
+});
---
+++ TriviaGame/assets/javascript/questions.js
@@ -0,0 +1,123 @@
+let trivia = [
+    {
+        question: `Sells exotic weapons and gear to Guardians`,
+        choices: {
+            a: `Cayde-6`,
+            b: `The Colonel`,
+            c: `Siviks`,
+            d: `Xur`
+        },
+        answer: `d`,
+        gif: `TriviaGame/../../images/xur (1).gif`,
+        wrong: `TriviaGame/../../images/giphy (1).gif`,
+        colors: ['red', 'gold', 'teal', 'purple']
+    },
+    {
+        question: `Who gifted the hand cannon "The Last Word" to Guardians? `,
+        choices: {
+            a: `Shaxx`,
+            b: `Shin Malphur`,
+            c: `Oryx`,
+            d: `Eva Levante`
+        },
+        answer: `b`,
+        gif: `TriviaGame/../../images/Animated-GIF-downsized_large.gif`,
+        wrong: `TriviaGame/../../images/giphy (1).gif`
+    },
+    {
+        question: `What was the most desired weapon in Destiny year 1?`,
+        choices: {
+            a: `Gjallarhorn`,
+            b: `Suros Regime`,
+            c: `Thorn`,
+            d: `Thunderlord`
+        },
+        answer: `a`,
+        gif: `TriviaGame/../../images/giphy.gif`,
+        wrong: `TriviaGame/../../images/giphy (1).gif`
+    },
+    {
+        question: `What was the name of the skeleton found in the Vault of Glass?`,
+        choices: {
+            a: `Kabr, The Legionless`,
+            b: `Praedyth, the Warlock`,
+            c: `Pahanin, the Hunter `,
+            d: `Dredgen Yor`
+        },
+        answer: `b`,
+        gif: `TriviaGame/../../images/giphy (2).gif`,
+        wrong: `TriviaGame/../../images/giphy (1).gif`
+    },
+    {
+        question: `What is the Queen's real name?`,
+        choices: {
+            a: `Eva Levante`,
+            b: `Mara Sov`,
+            c: `Ikora Rey`,
+            d: `Eris Morn`
+        },
+        answer: `b`,
+        gif: `TriviaGame/../../images/giphy (3).gif`,
+        wrong: `TriviaGame/../../images/giphy (1).gif`
+    },
+    {
+        question: `Where can you find this lore "I am not used by many. Yet I help all who are most mobile. What are guns? What is Light? I know only physicality. Be careful, I can help two but not three"?`,
+        choices: {
+            a: `Gjallarhorn`,
+            b: `Le Monarque`,
+            c: `Stomp-EE5`,
+            d: `Malfesance`
+        },
+        answer: `c`,
+        gif: `TriviaGame/../../images/couOVCp.jpg`,
+        wrong: `TriviaGame/../../images/giphy (1).gif`
+    },
+    {
+        question: `Who was the first enemy you face in the Last Wish raid?`,
+        choices: {
+            a: `Riven of a Thousand Voices`,
+            b: `Morgeth, the Spirekeeper`,
+            c: `Suro Chi, the Corrupted`,
+            d: `Kalli, the Corrupted`
+        },
+        answer: `d`,
+        gif: `assets/images/bingo.gif`,
+        wrong: `TriviaGame/../../images/giphy (1).gif`
+    },
+    {
+        question: `Where can you find this flavor text "Wait for enemy to make a mistake. Die. Stand by for Ghost Resurraction. Repeat as necessary."`,
+        choices: {
+            a: `Ruin Wings`,
+            b: `Fabian Strategy`,
+            c: `Cloak of Tekmor`,
+            d: `Ghost Angel cloak`
+        },
+        answer: `b`,
+        gif: `TriviaGame/../../images/Maxresdefault.jpg`,
+        wrong: `TriviaGame/../../images/giphy (1).gif`
+    },
+    {
+        question: `What medal can you win after defeating 25 opposing Guardians without dying, kill feed states "Someone should probably do something about..."`,
+        choices: {
+            a: `We Ran out of Medals`,
+            b: `At Any Cost`,
+            c: `Never Speak of This Again`,
+            d: `Nail in the Coffin`
+        },
+        answer: `a`,
+        gif: `TriviaGame/../../images/qnB.gif`,
+        wrong: `TriviaGame/../../images/giphy (1).gif`
+    },
+    {
+        question: `What Hunter subclass ability allows Tethered enemies to explode when killed. Damage to tethered enemies is shared to all?`,
+        choices: {
+            a: `Blackhole`,
+            b: `Envenomed`,
+            c: `Blood Bound`,
+            d: `Shadowshot`
+        },
+        answer: `c`,
+        gif: `TriviaGame/../../images/giphy (4).gif`,
+        wrong: `TriviaGame/../../images/giphy (1).gif`
+    }
+];
